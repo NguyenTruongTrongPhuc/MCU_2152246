befa23ca62760fa12afe3293f9a409e832897779
phuc.nguyenhcmutk21@hcmut.edu.vn <phuc.nguyenhcmutk21@hcmut.edu.vn>
2024-09-17 16:19:06 +0700

    Sample commit

---
+++ MCU_lab1-Ex5/MCU_lab1-Ex5/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 23, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+
+extern int timer_flag;
+void setTimer(int duration);
+void timerRun();
+#endif /* INC_SOFTWARE_TIMER_H_ */
